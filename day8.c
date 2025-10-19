935bee475cf0ffe6ecb357bacf8c414f32d4d458
raidenmei20061129-afk <raidenmei20061129@gmail.com>
2025-10-19 17:17:23 +0800

    Add files via upload

---
+++ day8.c
@@ -0,0 +1,18 @@
+//#define _CRT_SECURE_NO_WARNINGS
+//#include<stdio.h>
+////初见函数
+////函数模块化，可以简便代码的编写
+//int Add(int x, int y)//函数Add的声明
+//{
+//	int z = 0;
+//	z = x + y;
+//	return z;
+//}
+//int main() {
+//	int num1=0;
+//	int num2=0;
+//	scanf("%d %d", &num1, &num2);
+//	int sam = Add(num1, num2);
+//	printf("%d", sam);
+//	return 0;
+//}
